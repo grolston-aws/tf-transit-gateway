da4ec2abcbeb852dd447766a428a0fa74147e052
George Rolston <grolston@outlook.com>
2021-08-09 21:37:08 -0600

    init

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "az1" {
+  default = "us-west-2a"
+}
+
+variable "az2" {
+  default = "us-west-2b"
+}
+
+variable "scenario" {
+  default = "poc-tgw"
+}
