0f8fbe81b355ab2132606ddc4c0f5fa5a61d0b49
George Rolston <grolston@outlook.com>
2021-08-16 15:57:15 -0600

    splitting out regions

---
+++ terraform/us-east-1/main.tf
@@ -0,0 +1,185 @@
+
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate-netops"
+    key    = "tgw-us-east-1/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+
+
+###########################
+# Transit Gateway Section #
+###########################
+
+# Transit Gateway
+## Default association and propagation are disabled since our scenario involves
+## a more elaborated setup where
+## - Dev and Stage VPC can reach themselves and the Shared VPC
+## - the Shared VPC can reach all VPCs
+## - the Prod VPC can only reach the Shared VPC
+## The default setup being a full mesh scenario where all VPCs can see every other
+resource "aws_ec2_transit_gateway" "poc-tgw" {
+  description                     = "Transit Gateway testing scenario with 4 VPCs, 2 subnets each"
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+
+  tags = {
+    Name     = "${var.scenario}"
+    scenario = "${var.scenario}"
+  }
+}
+
+# VPC attachment
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-vpc-dev" {
+  subnet_ids                                      = ["${aws_subnet.vpc-dev-sub-a.id}", "${aws_subnet.vpc-dev-sub-b.id}"]
+  transit_gateway_id                              = aws_ec2_transit_gateway.poc-tgw.id
+  vpc_id                                          = aws_vpc.vpc-dev.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags = {
+    Name     = "tgw-att-vpc-dev"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-vpc-stage" {
+  subnet_ids                                      = ["${aws_subnet.vpc-stage-sub-a.id}", "${aws_subnet.vpc-stage-sub-b.id}"]
+  transit_gateway_id                              = aws_ec2_transit_gateway.poc-tgw.id
+  vpc_id                                          = aws_vpc.vpc-stage.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags = {
+    Name     = "tgw-att-vpc-stage"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-vpc-shared" {
+  subnet_ids                                      = ["${aws_subnet.vpc-shared-sub-a.id}", "${aws_subnet.vpc-shared-sub-b.id}"]
+  transit_gateway_id                              = aws_ec2_transit_gateway.poc-tgw.id
+  vpc_id                                          = aws_vpc.vpc-shared.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags = {
+    Name     = "tgw-att-vpc-shared"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-vpc-prod" {
+  subnet_ids                                      = ["${aws_subnet.vpc-prod-sub-a.id}", "${aws_subnet.vpc-prod-sub-b.id}"]
+  transit_gateway_id                              = aws_ec2_transit_gateway.poc-tgw.id
+  vpc_id                                          = aws_vpc.vpc-prod.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags = {
+    Name     = "tgw-att-vpc-prod"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+# Route Tables
+
+resource "aws_ec2_transit_gateway_route_table" "tgw-dev-rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.poc-tgw.id
+  tags = {
+    Name     = "tgw-dev-rt"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+resource "aws_ec2_transit_gateway_route_table" "tgw-shared-rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.poc-tgw.id
+  tags = {
+    Name     = "tgw-shared-rt"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+resource "aws_ec2_transit_gateway_route_table" "tgw-prod-rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.poc-tgw.id
+  tags = {
+    Name     = "tgw-prod-rt"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+# Route Tables Associations
+## This is the link between a VPC (already symbolized with its attachment to the Transit Gateway)
+## and the Route Table the VPC's packet will hit when they arrive into the Transit Gateway.
+## The Route Tables Associations do not represent the actual routes the packets are routed to.
+## These are defined in the Route Tables Propagations section below.
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-vpc-dev-assoc" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-dev.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-dev-rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-vpc-stage-assoc" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-stage.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-dev-rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-vpc-shared-assoc" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-shared.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-shared-rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-vpc-prod-assoc" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-prod.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-prod-rt.id
+}
+
+# Route Tables Propagations
+## This section defines which VPCs will be routed from each Route Table created in the Transit Gateway
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-dev-to-vpc-dev" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-dev.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-dev-rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-dev-to-vpc-stage" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-stage.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-dev-rt.id
+}
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-dev-to-vpc-shared" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-shared.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-dev-rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-shared-to-vpc-dev" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-dev.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-shared-rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-shared-to-vpc-stage" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-stage.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-shared-rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-shared-to-vpc-prod" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-prod.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-shared-rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-prod-to-vpc-shared" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-shared.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-prod-rt.id
+}
+
+# ROUTES
---
+++ terraform/us-east-1/variables.tf
@@ -0,0 +1,16 @@
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "az1" {
+  default = "us-east-1a"
+}
+
+variable "az2" {
+  default = "us-east-1b"
+}
+
+variable "scenario" {
+  default = "poc-tgw"
+}
