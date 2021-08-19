e38f3d56e4fa80050f70ee7957a8d7bb31a598c5
George Rolston <grolston@users.noreply.github.com>
2021-08-19 12:30:53 -0600

    Fb us east 2 (#3)
    * init east2
    
    * formatting
    
    * doc update

---
+++ terraform/us-east-2/main.tf
@@ -0,0 +1,42 @@
+
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate-netops"
+    key    = "tgw-us-east-2/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+###########################
+# Transit Gateway Section #
+###########################
+
+## Transit Gateway
+resource "aws_ec2_transit_gateway" "poc-tgw" {
+  description                     = "US-EAST-2 TGW POC"
+  default_route_table_association = "enable"
+  default_route_table_propagation = "enable"
+  amazon_side_asn                 = 64534
+  auto_accept_shared_attachments  = "enable"
+  dns_support                     = "enable"
+
+  tags = {
+    Name     = "${var.scenario}"
+    scenario = "${var.scenario}"
+  }
+}
+
+resource "aws_ec2_transit_gateway_peering_attachment" "poc-tgw-attach-to-west-2" {
+  peer_account_id         = aws_ec2_transit_gateway.poc-tgw.owner_id ## both TGWs in same account
+  peer_region             = "us-west-2"
+  peer_transit_gateway_id = "tgw-03fea9685797c5865"
+  transit_gateway_id      = aws_ec2_transit_gateway.poc-tgw.id
+
+  tags = {
+    Name = "EAST2 TGW Peering Requestor WEST2"
+  }
+}
---
+++ terraform/us-east-2/tgw-rt-shared.tf
@@ -0,0 +1,37 @@
+resource "aws_ec2_transit_gateway_route_table" "tgw-shared-rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.poc-tgw.id
+  tags = {
+    Name     = "tgw-shared-rt"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+# Route Tables Associations
+## This is the link between a VPC (already symbolized with its attachment to the Transit Gateway)
+## and the Route Table the VPC's packet will hit when they arrive into the Transit Gateway.
+## The Route Tables Associations do not represent the actual routes the packets are routed to.
+## These are defined in the Route Tables Propagations section below.
+
+# resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-vpc-shared-assoc" {
+#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-shared.id
+#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-shared-rt.id
+# }
+
+# Route Tables Propagations
+## This section defines which VPCs will be routed from each Route Table created in the Transit Gateway
+
+# resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-shared-to-vpc-dev" {
+#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-dev.id
+#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-shared-rt.id
+# }
+
+# resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-shared-to-vpc-stage" {
+#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-stage.id
+#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-shared-rt.id
+# }
+
+# resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-shared-to-vpc-prod" {
+#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att-vpc-prod.id
+#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-shared-rt.id
+# }
