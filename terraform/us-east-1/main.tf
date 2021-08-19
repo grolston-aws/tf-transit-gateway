57b03c3f5f366b8dd1bd20bb7331bc0eae3d8543
George Rolston <grolston@outlook.com>
2021-08-19 14:01:30 -0600

    cleanup

--- terraform/us-east-1/main.tf
+++ terraform/us-east-1/main.tf
@@ -41,3 +41,21 @@ resource "aws_ec2_transit_gateway_peering_attachment" "poc-tgw-attach-to-west-2"
     Name = "EAST1 TGW Peering Requestor WEST2"
   }
 }
+
+resource "aws_ec2_transit_gateway_route_table" "tgw-prod-rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.poc-tgw.id
+  tags = {
+    Name     = "tgw-prod-rt"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
+
+resource "aws_ec2_transit_gateway_route_table" "tgw-nonprod-rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.poc-tgw.id
+  tags = {
+    Name     = "tgw-nonprod-rt"
+    scenario = "${var.scenario}"
+  }
+  depends_on = ["aws_ec2_transit_gateway.poc-tgw"]
+}
