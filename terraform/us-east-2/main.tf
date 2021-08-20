2e9c2f0a97184336f1c50a2bd218eb0e0a6e9111
George Rolston <grolston@users.noreply.github.com>
2021-08-20 13:44:03 -0600

    accept (#9)

--- terraform/us-east-2/main.tf
+++ terraform/us-east-2/main.tf
@@ -37,6 +37,14 @@ resource "aws_ec2_transit_gateway_peering_attachment" "poc-tgw-attach-to-west-2"
   }
 }
 
+resource "aws_ec2_transit_gateway_peering_attachment_accepter" "accept_peer_us_east_1" {
+  transit_gateway_attachment_id = "tgw-attach-0d379323b40e1cabc"
+
+  tags = {
+    Name = "US-EAST-1 US-EAST-2"
+  }
+}
+
 resource "aws_ec2_transit_gateway_route_table" "tgw-prod-rt" {
   transit_gateway_id = aws_ec2_transit_gateway.poc-tgw.id
   tags = {
