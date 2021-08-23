fd3587610947562ced5f7cece3790ae9513e9797
George Rolston <grolston@outlook.com>
2021-08-22 21:30:55 -0600

    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,12 +12,12 @@ terraform {
 
 # US-WEST2 accepts the Peering attachment.
 provider "aws" {
-  alias = "west2"
+  alias  = "west2"
   region = var.region
 }
 
 provider "aws" {
-  alias = "east1"
+  alias  = "east1"
   region = "us-east-1"
 }
 
@@ -40,7 +40,7 @@ data "aws_caller_identity" "east2" {
 
 ## US-WEST-2
 resource "aws_ec2_transit_gateway" "poc_tgw_west2" {
-  provider = aws.west2
+  provider                        = aws.west2
   description                     = "US-WEST-2 TGW POC"
   default_route_table_association = "enable"
   default_route_table_propagation = "enable"
@@ -49,21 +49,21 @@ resource "aws_ec2_transit_gateway" "poc_tgw_west2" {
   dns_support                     = "enable"
 
   tags = {
-    Name     = "US-WEST-2 TGW POC"
+    Name = "US-WEST-2 TGW POC"
   }
 }
 
 resource "aws_ec2_transit_gateway_route_table" "tgw_prod_rt" {
   transit_gateway_id = aws_ec2_transit_gateway.poc-tgw-west2.id
   tags = {
-    Name     = "tgw-prod-rt"
+    Name = "tgw-prod-rt"
   }
   depends_on = ["aws_ec2_transit_gateway.poc_tgw_west2"]
 }
 
 ## US-EAST-1
 resource "aws_ec2_transit_gateway" "poc_tgw_east1" {
-  provider = aws.east1
+  provider                        = aws.east1
   description                     = "US-EAST-1 TGW POC"
   default_route_table_association = "enable"
   default_route_table_propagation = "enable"
@@ -72,14 +72,14 @@ resource "aws_ec2_transit_gateway" "poc_tgw_east1" {
   dns_support                     = "enable"
 
   tags = {
-    Name     = "US-EAST-1 TGW POC"
+    Name = "US-EAST-1 TGW POC"
   }
 }
 
 resource "aws_ec2_transit_gateway_route_table" "tgw_prod_rt_e1" {
   transit_gateway_id = aws_ec2_transit_gateway.poc_tgw_east1.id
   tags = {
-    Name     = "tgw-prod-rt-e1"
+    Name = "tgw-prod-rt-e1"
   }
   depends_on = ["aws_ec2_transit_gateway.poc_tgw_east1"]
 }
@@ -97,14 +97,14 @@ resource "aws_ec2_transit_gateway" "poc_tgw_east2" {
   dns_support                     = "enable"
 
   tags = {
-    Name     = "US-EAST-2 TGW POC"
+    Name = "US-EAST-2 TGW POC"
   }
 }
 
 resource "aws_ec2_transit_gateway_route_table" "tgw_prod_rt_e2" {
   transit_gateway_id = aws_ec2_transit_gateway.poc_tgw_east2.id
   tags = {
-    Name     = "tgw-prod-rt-e2"
+    Name = "tgw-prod-rt-e2"
   }
   depends_on = ["aws_ec2_transit_gateway.poc_tgw_east2"]
 }
